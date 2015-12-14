aab6863920a8f384c24061a0a728ba1623fbbbd6
David <david.dunkley@euromoneyplc.com>
2015-12-14 09:46:15 +0000

    Created SpecFlow Api Acceptance Test
    Created an Acceptance Test using SpecFlow.
    Uses Mock Api returning standard Http Response Codes.

---
+++ SpecFlowPlaying/MyApiAcceptanceTests/ApiResponses.feature
@@ -0,0 +1,49 @@
+﻿Feature: ApiResponses
+
+	In order to act on the outcome of an API request
+	As an Client who sends a request
+	I want to be told the outcome of the request
+
+@mytag
+Scenario: Request which is Successful
+	Given I have a successful request
+	And a user with the following:
+	| name | email              |
+	| rob  | rob@mission202.com |
+	When I call the Api
+	Then the response has a status code of 200
+
+Scenario: Request which Creates an Item
+	Given I have a successful request
+	And It also creates an item
+	When I call the Api
+	Then the response has a status code of 201
+
+Scenario: Request which is Performed Offline
+	Given I have a successful request
+	And It is also performed offline
+	When I call the Api
+	Then the response has a status code of 202
+
+Scenario: Request which Returns No Content when it is successful
+	Given I have a successful request
+	And It also returns no content
+	When I call the Api
+	Then the response has a status code of 204
+
+Scenario: Request which is badly formed
+	Given I have a request that should not be sent again
+	And It is also badly formed
+	When I call the Api
+	Then the response has a status code of 400
+
+Scenario: Requests which already exists
+	Given I have a request that should not be sent again
+	And It also has a conflict
+	When I call the Api
+	Then the response has a status code of 409
+
+Scenario: Requests where an internal error occurs
+	Given I have a request that fails but should be sent again
+	When I call the Api
+	Then the response has a status code of 500
---
+++ SpecFlowPlaying/MyApiAcceptanceTests/ApiResponsesSteps.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Http;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace MyApiAcceptanceTests
+{
+    [Binding]
+    public class ApiResponsesSteps
+    {
+        private HttpClient _httpClient;
+        private int _expectedResponseCode;
+        private HttpResponseMessage _httpResponseMessage;
+
+        [Given(@"I have a successful request")]
+        public void GivenIHaveASuccessfulRequest()
+        {
+            _expectedResponseCode = 200;
+        }
+
+        [Given(@"a user with the following:")]
+        public void GivenAUserWithTheFollowing(Table table)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"It also creates an item")]
+        public void GivenItAlsoCreatesAnItem()
+        {
+            _expectedResponseCode = 201;
+        }
+
+        [Given(@"It is also performed offline")]
+        public void GivenItIsAlsoPerformedOffline()
+        {
+            _expectedResponseCode = 202;
+        }
+
+        [Given(@"It also returns no content")]
+        public void GivenItAlsoReturnsNoContent()
+        {
+            _expectedResponseCode = 204;
+        }
+
+        [Given(@"I have a request that should not be sent again")]
+        public void GivenIHaveARequestThatShouldNotBeSentAgain()
+        {
+            _expectedResponseCode = 400;
+        }
+
+        [Given(@"It is also badly formed")]
+        public void GivenItIsAlsoBadlyFormed()
+        {
+            _expectedResponseCode = 400;
+        }
+
+        [Given(@"It also has a conflict")]
+        public void GivenItAlsoHasAConflict()
+        {
+            _expectedResponseCode = 409;
+        }
+
+        [Given(@"I have a request that fails but should be sent again")]
+        public void GivenIHaveARequestThatFailsButShouldBeSentAgain()
+        {
+            _expectedResponseCode = 500;
+        }
+
+        [When(@"I say (.+)")]
+        public void WhenISayWTF(string message)
+        {
+            Debug.WriteLine(message);
+        }
+
+        [When(@"I call the Api")]
+        public void WhenICallTheApi()
+        {
+            _httpClient = new HttpClient()
+            {
+                BaseAddress = new Uri("http://httpstat.us/"),
+            };
+
+            _httpResponseMessage = _httpClient.PostAsync($"{_expectedResponseCode}", null).Result;
+        }
+
+        [Then(@"the response has a status code of (\d{3})")]
+        public void ThenTheResponseHasAStatusCodeOf(int statusCode)
+        {
+            var expected = (HttpStatusCode)statusCode;
+            var actual = _httpResponseMessage.StatusCode;
+
+            if (expected == actual)
+            {
+                Debug.Print($"Received expected response of {(int)expected} ({expected}).");
+            }
+            else
+                Assert.Fail($"Received unexpected response of {(int)actual} ({actual}).");
+        }
+    }
+}
